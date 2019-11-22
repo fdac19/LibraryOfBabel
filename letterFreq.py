8fdec80055787acaa69d7d0e7a8af6d0239520c6
luzhixiu1996 <luzhixiu1996@gmail.com>
2019-11-22 10:20:56 -0500

    one more update

--- letterFreq.py
+++ letterFreq.py
@@ -15,11 +15,14 @@ import matplotlib.pyplot as plt
 def letterFreq(wholeString,yLabel="Letter Frequencies",title="title"):
     alphabeltString=string.ascii_lowercase
     alphabetList=[]
+    wholeString= filter(str.isalpha, wholeString) 
     for letter in alphabeltString:
         alphabetList.append(letter)
     letterCntDict=dict()
+    letterFreqDict=dict()
     for letter in alphabeltString:
         letterCntDict[letter]=wholeString.count(letter)
+        letterFreqDict[letter]=float(wholeString.count(letter))/len(wholeString)
     sorted_x = sorted(letterCntDict.items(), key=lambda x: x[1])
     keyList=[]
     cntList=[]
@@ -30,16 +33,16 @@ def letterFreq(wholeString,yLabel="Letter Frequencies",title="title"):
         cntList.append(int(tup[1]))
 
     
-    objects = keyList
-    y_pos = np.arange(len(objects))
-    performance = cntList
-    plt.figure()
-    plt.bar(y_pos, performance, align='center', alpha=0.5)
-    plt.xticks(y_pos, objects)
-    plt.ylabel(yLabel)
-    plt.title(title)
+#    objects = keyList
+#    y_pos = np.arange(len(objects))
+#    performance = cntList
+#    plt.figure()
+#    plt.bar(y_pos, performance, align='center', alpha=0.5)
+#    plt.xticks(y_pos, objects)
+#    plt.ylabel(yLabel)
+#    plt.title(title)
 #    plt.show()
-    return letterCntDict
+    return letterFreqDict
     
 wordList=[]
 
@@ -61,14 +64,60 @@ def getStringFromFile(fname):
 text_10000= getStringFromFile("wiki-100k.txt")
 letterFreq(text_10000)
 
-for i in [100,200,400,800,1600,3200,6400,12800,25600,51200,1000000]:
+collectionList=[]
+plotMatrix=[]
+
+samples=[]
+for i in range(1,100):
+    samples.append(i*100)
+
+#samples=[100,200,400,800,1600,3200,6400,12800,]
+for i in samples:
     print "Top %d"%i
     partList=wordList[:i]
     testString=""
     for word in partList:
         testString+=word
     letterCntDict = letterFreq(testString)
-    print letterCntDict
+    letterFreqDict=dict()
+    plotList=[]
+    for key in letterCntDict:
+        cnt=letterCntDict[key]
+        freq=cnt/float(i)
+        letterFreqDict[key]=freq
+#    print letterFreqDict
+    collectionList.append(letterFreqDict)
+    
+#freqMatrix=[] #each item in freqMatrix is character, going from a to z    
+#plt.figure()
+#alphabeltString="abcdefghijklmnopqrstuvwxyz"
+#for letter in alphabeltString:
+#    letterFreq=[]
+#    for freqDict in collectionList:
+#        letterFreq.append(freqDict[letter])
+##    print letterFreq
+#    plt.plot(samples[:100],letterFreq[:100],label=letter)
+#ax = plt.gca()
+#plt.xlabel("Most Common Words")
+#plt.ylabel("Letter Frequency")
+#plt.legend(bbox_to_anchor=(1.2, 1.1), bbox_transform=ax.transAxes) 
+#plt.show()
+#
+#plt.figure()
+#
+#alphabeltString="eh"
+#for letter in alphabeltString:
+#    letterFreq=[]
+#    for freqDict in collectionList:
+#        letterFreq.append(freqDict[letter])
+##    print letterFreq
+#    plt.plot(samples,letterFreq,label=letter)
+#ax = plt.gca()
+#plt.xlabel("Most Common Words")
+#plt.ylabel("Letter Frequency")
+#plt.legend(bbox_to_anchor=(1.2, 0.5), bbox_transform=ax.transAxes) 
+#plt.show()
+#
 
 
 
@@ -77,24 +126,35 @@ for i in [100,200,400,800,1600,3200,6400,12800,25600,51200,1000000]:
 
 
 #
-#grim= getStringFromFile("10000.txt")
-#letterFreq(grim,title="Letter Usage in Grim's Fairy Tail")
-#
-#grim= getStringFromFile("pp.txt")
-#letterFreq(grim,title="Letter Usage in Pride and Prejudice")
-#
-#
-#grim= getStringFromFile("paper.txt")
-#letterFreq(grim,title="Letter Usage in Acdemic Paper Collection")
-#
-#grim= getStringFromFile("copyRightLaw.txt")
-#letterFreq(grim,title="Letter Usage in Copy Right Law")
-#
-#grim= getStringFromFile("wiki-100k.txt")
-#letterFreq(grim,title="Letter Usage in Acdemic Paper Collection")
+grim= getStringFromFile("10000.txt")
+newDict= letterFreq(grim,title="Letter Usage in Grim's Fairy Tail")
+print newDict['h']
+print newDict['e']
+print
 
+grim= getStringFromFile("pp.txt")
+newDict= letterFreq(grim,title="Letter Usage in Pride and Prejudice")
+print newDict['h']
+print newDict['e']
+print
 
+grim= getStringFromFile("paper.txt")
+newDict= letterFreq(grim,title="Letter Usage in Acdemic Paper Collection")
+print newDict['h']
+print newDict['e']
+print 
 
+grim= getStringFromFile("copyRightLaw.txt")
+newDict= letterFreq(grim,title="Letter Usage in Copy Right Law")
+print newDict['h']
+print newDict['e']
+print 
+
+grim= getStringFromFile("grim.txt")
+newDict= letterFreq(grim,title="Letter Usage in Acdemic Paper Collection")
+print newDict['h']
+print newDict['e']
+print
 
 
 #print wholeString
